db7885a1787d2680e66a9aad84583176e98ad820
Jamie <jmaniulit@csumb.edu>
2021-12-15 15:53:44 -0800

    Implemented API, displayed data on home and pet page

--- home/main.py
+++ home/main.py
@@ -1,17 +1,42 @@
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
+import requests
+import random
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+
+images = requests.get("https://dog.ceo/api/breed/hound/images")
+imgData = images.json()['message']
+
+breeds = requests.get("https://dog.ceo/api/breed/hound/list")
+breedData = breeds.json()['message']
+
 @app.route('/')
 def home():
-    return render_template('home.html')
-
+    firstBreed = random.choice(imgData)
+    secondBreed = random.choice(imgData)
+    thirdBreed = random.choice(imgData)
+    return render_template('home.html',data1=firstBreed,data2=secondBreed,data3=thirdBreed)
 
 @app.route('/findPet')
-def petSearch():
-    return render_template('findPet.html')
+def petFind():
+    breed1 = breedData[0]
+    img1 = imgData[0]
+    breed2 = breedData[1]
+    img2 = imgData[242]
+    breed3 = breedData[2]
+    img3= imgData[416]
+    breed4 = breedData[3]
+    img4 = imgData[609]
+    breed5 = breedData[4]
+    img5 = imgData[760]
+    breed6 = breedData[5]
+    img6 = imgData[953]
+    return render_template('findPet.html', b1=breed1, i1=img1,b2=breed2, i2=img2,
+                           b3=breed3, i3=img3,b4=breed4, i4=img4,b5=breed5, i5=img5, b6=breed6, i6=img6)
 
 @app.route('/signUp')
 def CreateAcc():
